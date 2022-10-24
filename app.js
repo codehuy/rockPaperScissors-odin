9de32110455f98a2f5ab845ea5c22a89b02a0eb2
Huy <64755394+codehuy@users.noreply.github.com>
2022-10-24 16:49:58 -0400

    Added playRound function

--- app.js
+++ app.js
@@ -3,5 +3,17 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)];
 }
 
-
+function playRound(playerSelection, compSelection){
+    if (playerSelection === compSelection) {
+        return `Its a Tie! You chose ${playerSelection} and Computer chose ${compSelection}`
+    } else if (playerSelection === 'rock' && compSelection === 'scissors'){
+        return 'You win! Rock Beats Scissors.'
+    } else if (playerSelection === 'paper' && compSelection === 'rock'){
+        return "You win! Paper beats Rock"
+    } else if (playerSelection === 'scissors' && compSelection === 'paper') {
+        return "You win! Scissors beat Paper"
+    } else {
+        return `You lose.  ${compSelection} beats ${playerSelection}`
+    }
+}
 
